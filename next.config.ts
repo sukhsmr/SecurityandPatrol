e9575b9373cd4eaf7acee6dda3d9f2d4204ee0d8
Ronakjadav43 <ronakjadav.tech@gmail.com>
2026-09-20 07:33:45 +0530

    refactor: migrate from MySQL to static JSON and enable static export

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
